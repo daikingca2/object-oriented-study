0799c820180c3f06062c8957dadea7be6f727936
d_ojima <d_ojima@aroba.jp>
2016-02-09 22:35:13 +0900

    02を追加

--- 01_objective-helloworld/helloworld.php
+++ 01_objective-helloworld/helloworld.php
@@ -1,45 +1,29 @@
-// 01_objective-helloworld
 <?php
+//  2016/02/03 Dev-study
+// 01_objective-helloworld
 
-class helloworld {
+// print_backtrace(debug_backtrace());
+// debug
+// var_dump("hoge");
+class helloworld
+{
   // class helloworld
-  public $strHelloworld = "Hello World";
+  public $strHelloworld;
+  // ↑外部から書き換えられるようになってしまっている
+  // こうかいにするべきではない
 
-  function getHelloworld() {
-    echo $this->$strHelloworld;
+  function getHelloworld()
+  {
+    // echo ($this->$strHelloworld);
+    // echo $this->$strHelloworld;
+    echo "hellohoge";
   }
-
-  $hello = new helloworld;
-  $hello->getHelloworld();
 
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  $hello = new helloworld;
+  $hello->getHelloworld = "Hello World!";
+  $hello->getHelloworld();
+// echo "hoge";
 
   ?>
---
+++ 02_saisenbako/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<!-- コイン専用賽銭箱 -->
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>02_saisenbako</title>
+  </head>
+  <body>
+    <h1>コイン専用賽銭箱</h1>
+    <div class="base">
+      <form class="coin_input" action="index.html" method="post">
+        <p>金額を入力して下さい。</p>
+        <input type="text" name="name" value="">
+        <input type="submit" name="name" value="投げる">
+      </form>
+      <p></p>
+      <button type="button" name="button">回収</button>
+
+    </div>
+  <?php
+
+
+
+
+
+
+   ?>
+  </body>
+</html>
